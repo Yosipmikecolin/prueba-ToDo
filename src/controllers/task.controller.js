9ca899c16e58464b13845c6c890b1ca92f227f35
Yosip Mike Colin <colinparrado@gmail.com>
2024-08-24 16:10:58 -0500

    Create tasks

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-export const controllerGET = (req, res) => {
+export const controllerGET = (_req, res) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const filePath = path.join(__dirname, "../database/tasks.json");
@@ -18,5 +18,52 @@ export const controllerGET = (req, res) => {
         res.json(JSON.parse("[]"));
       }
     }
+  });
+};
+
+export const controllerPOST = (req, res) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "../database/tasks.json");
+  const { title } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ error: "Title are required" });
+  }
+
+  // * READ THE FILE tasks.json
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error reading file");
+    }
+
+    let tasks;
+    try {
+      tasks = JSON.parse(data);
+    } catch (parseErr) {
+      console.error(parseErr);
+      return res.status(500).send("Error parsing JSON");
+    }
+
+    // * CREATE A NEW TASK
+    const newTask = {
+      id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      title,
+    };
+
+    // * ADD THE NEW TASK TO THE TASK ARRAY
+    tasks.push(newTask);
+
+    // * WRITE UPDATED TASKS TO THE FILE
+    fs.writeFile(filePath, JSON.stringify(tasks, null, 2), (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr);
+        return res.status(500).send("Error writing file");
+      }
+
+      // * SEND SUCCESS RESPONSE
+      res.status(201).json(newTask);
+    });
   });
 };
--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { controllerGET } from "../controllers/task.controller.js";
+import {
+  controllerGET,
+  controllerPOST,
+} from "../controllers/task.controller.js";
 
 const route = Router();
 
 route.get("/tasks", controllerGET);
-route.post("/tasks", controllerGET);
+route.post("/tasks", controllerPOST);
 route.put("/tasks/:id", controllerGET);
 route.delete("/tasks/:id", controllerGET);
 
